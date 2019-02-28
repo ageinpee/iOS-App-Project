abec04a75eb9bff1c01d49248f95283c4d35f2dc
Henrik Peters <henrikpeters@Henriks-MacBook-Air.local>
2019-02-28 14:53:57 +0100

    initial project file added

---
+++ SinglePhotoApp/SinglePhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhotoApp
+//
+//  Created by Henrik Peters on 28.02.19.
+//  Copyright © 2019 Henrik Peters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
