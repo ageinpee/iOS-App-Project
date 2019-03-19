571cf4b0792c4e757ad6313d8a8349df132ed8c9
ageinpee <henrik.peters.98@web.de>
2019-03-19 17:59:04 +0100

    started a basic tableview. more customization is yet to come

---
+++ UIKitCustomizations/UIKitCustomization/UIKitCustomization/First_TableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  FirstViewController.swift
+//  UIKitCustomization
+//
+//  Created by Henrik Peters on 19.03.19.
+//  Copyright © 2019 Henrik Peters. All rights reserved.
+//
+
+import UIKit
+
+class First_TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let data = ["first", "second", "third", "fourth"]
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(data[indexPath.row])"
+        return cell
+    }
+}
+
+
+class CustomTableViewCell: UITableViewCell {
+    
+}
+
+
+
+extension UIView {
+    
+    func anchor (top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat, enableInsets: Bool) {
+        var topInset = CGFloat(0)
+        var bottomInset = CGFloat(0)
+        
+        if #available(iOS 11, *), enableInsets {
+            let insets = self.safeAreaInsets
+            topInset = insets.top
+            bottomInset = insets.bottom
+            
+            print("Top: \(topInset)")
+            print("bottom: \(bottomInset)")
+        }
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop+topInset).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom-bottomInset).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+    }
+}
---
+++ UIKitCustomizations/UIKitCustomization/UIKitCustomization/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  UIKitCustomization
+//
+//  Created by Henrik Peters on 19.03.19.
+//  Copyright © 2019 Henrik Peters. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
